b005544d515d1c90e928ab0613c04113cf409126
전윤호 Uknow <yoon6763@naver.com>
2025-02-03 00:20:32 +0900

    프로젝트 생성

---
+++ src/main/java/com/test/teststudio/TeststudioApplication.java
@@ -0,0 +1,13 @@
+package com.test.teststudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeststudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeststudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/teststudio/TeststudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.teststudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeststudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
