c36402edf74f961dd998b88cfee128e518e4ad03
uknow-mac <yoon6763@naver.com>
2025-04-14 10:19:52 +0900

    AspectJ 설정

--- src/main/java/com/test/teststudio/TeststudioApplication.java
+++ src/main/java/com/test/teststudio/TeststudioApplication.java
@@ -2,12 +2,14 @@ package com.test.teststudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class TeststudioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeststudioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TeststudioApplication.class, args);
+    }
 
 }
